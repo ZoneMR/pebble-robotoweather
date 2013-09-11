0ccb40446fd7ed3a1c1c6010b69b13c52b38bd0f
Andrea Stagi <stagi.andrea@gmail.com>
2013-09-11 16:45:03 +0200

    SILENT mode in config.h

--- src/config.h
+++ src/config.h
@@ -1,4 +1,5 @@
 // Any of "us", "ca", "uk" (for idiosyncratic US, Candian and British measurements),
 // "si" (for pure metric) or "auto" (determined by the above latitude/longitude)
 #define UNIT_SYSTEM "auto"
+#define SILENT 1
 //#define DEBUG
